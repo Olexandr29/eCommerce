3aa63316558dfdcd644ecf344436493b17651af7
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2025-07-01 16:46:56 +0300

    Revert "Change forkflow and code for solving SessionNotCreated for Ubuntu with Chrome and Firefox"
    This reverts commit fa793276289455eac3d0127fb409b1fed7998474.

--- src/test/java/com/automation/tests/BaseTest.java
+++ src/test/java/com/automation/tests/BaseTest.java
@@ -2,12 +2,14 @@ package com.automation.tests;
 
 import com.automation.pages.*;
 import static com.automation.pages.TestData.*;
+
 import com.automation.tests.utils.DriverFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.PrintWriter;
 import java.time.Duration;
 
 public class BaseTest {
@@ -21,16 +23,43 @@ public class BaseTest {
     protected InventoryProductDetails inventoryProductDetails;
     protected CheckoutStep3CompletePage checkoutStep3CompletePage;
 
+  //  @BeforeAll
+
     @BeforeEach
     public void setUp() {
         String browser = System.getProperty("browser", "chrome");
         driver = DriverFactory.createDriver(browser);
+  //      DriverFactory.generateAllureEnvironment(driver); // ✅ ПРАВИЛЬНО
 
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get(url);
         loginPage = new LoginPage(driver, wait);
+
+    //    generateEnvironment(driver);
     }
+
+  /*  protected void generateEnvironment(WebDriver driver) {
+        if (driver == null) {
+            System.err.println("⚠  WebDriver is null. Skipping environment generation");
+        return;
+        }
+        try {
+            Capabilities capabilities = ((HasCapabilities) driver).getCapabilities();
+            //                    .getBrowserVersion();
+            String browserName = capabilities.getBrowserName();
+            String browserVersion = capabilities.getBrowserVersion();
+            //                     System.getProperty("browser", "chrome");
+
+        try (PrintWriter writer = new PrintWriter("allure-results/environment.properties")) {
+            writer.println("Browser=" + browserName);
+            writer.println("Browser.Version=" + browserVersion);
+        }
+        } catch (Exception e) {
+            System.err.println("❌ Could not generate Allure environment: " + e.getMessage());
+        }
+    }
+*/
 
     @AfterEach
     public void tearDown() {
