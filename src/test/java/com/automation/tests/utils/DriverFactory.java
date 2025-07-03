d5eed79273595ae1882c1e3f3a1fe71e967ffcca
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2025-07-03 12:00:59 +0300

    Changes for Linux&Firefox

--- src/test/java/com/automation/tests/utils/DriverFactory.java
+++ src/test/java/com/automation/tests/utils/DriverFactory.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
@@ -22,7 +23,15 @@ public class DriverFactory {
         switch (browserName.toLowerCase()) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+                String firefoxArgs = System.getProperty("firefox.options");
+                if (firefoxArgs != null) {
+                    for (String arg : firefoxArgs.split(" ")) {
+                        firefoxOptions.addArguments(arg);
+                    }
+                }
+                driver = new FirefoxDriver(firefoxOptions);
                 break;
             case "edge":
                 WebDriverManager.edgedriver().setup();
